e80ff72a1cd3633f5f76812039dffba1cc68219d
Mayank Nautiyal <mayanknautiyal0211@gmail.com>
2026-02-23 14:49:42 +0530

    into of python

---
+++ 01-basics/1_introd.py
@@ -0,0 +1,87 @@
+
+
+# 📘 Introduction to Python
+
+##? 1️⃣ What is Python?
+
+#! Python is a **high-level, interpreted, dynamically typed, object-oriented, general-purpose programming language**.
+
+#! It was created by Guido van Rossum in 1991.
+
+
+
+##? 2️⃣ Why Python is Popular
+
+#! Python is popular because:
+
+# * Simple syntax (easy to read)
+# * Huge community
+# * Massive library ecosystem
+# * Used in multiple domains
+
+# It dominates:
+
+# * Data Science
+# * Machine Learning
+# * Automation
+# * Backend development
+# * Scripting
+
+
+##? 3️⃣ High-Level Language
+
+#!High-level means:
+
+# * Human-readable
+# * No manual memory management
+# * No direct hardware control
+
+# Compare with low-level languages like C.
+
+
+
+## ?4️⃣ Interpreted Language
+
+#!Python code runs line by line through an interpreter.
+
+# !Unlike compiled languages like C++, which are compiled before running.
+
+# ?Note:
+# !Python is technically compiled to bytecode internally, but for beginners — think “interpreted.”
+
+
+
+# ? 5️⃣ Dynamically Typed
+
+#! You don’t declare types explicitly.
+
+
+x = 10
+x = "hello"
+
+
+# The type is decided at runtime.
+
+
+## ?6️⃣ Object-Oriented
+
+# !Everything in Python is an object.
+
+
+x = 10
+print(type(x))
+# output: <class 'int'>
+
+
+# Even integers are objects.
+
+## ?7️⃣ Multi-Paradigm
+
+# !Python supports:
+
+# * Procedural programming
+# * Object-oriented programming
+# * Functional programming
+
+# This flexibility is powerful.
+
